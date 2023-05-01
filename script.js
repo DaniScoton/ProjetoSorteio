e55dd856c0bf38a20d03f480afc5207a76217395
Dani Scoton <daniellyscoton@gmail.com>
2023-04-30 21:51:42 -0300

    alterações

--- script.js
+++ script.js
@@ -10,12 +10,40 @@ function adivinhar(){
     var escolhido = document.getElementById('escolhido').value;
     console.log(escolhido);
 
-    var parametro1 = valorFinalSelecionado - valorInicialSelecionado;
-    console.log(parametro1)
+    var mensagem = document.getElementById('msg');
 
-    var parametro2 = valorInicialSelecionado;
-    console.log(parametro2)
+    //validando parametros do usuário
 
-    var resultado = parseInt((Math.random() * parametro1)) + parametro2;
-    console.log('resultado: ' + resultado);
+    if(parseInt(valorInicialSelecionado) > parseInt(valorFinalSelecionado)){
+        mensagem.innerHTML = 'O Valor Inicial Deve ser Menor Que o Final';
+        mensagem.classList.remove('msg-certo');
+        mensagem.classList.add('msg-erro');
+    }else if(parseInt(escolhido) > parseInt(valorFinalSelecionado) || parseInt(escolhido) < parseInt(valorInicialSelecionado)){
+        mensagem.innerHTML = 'Número Inválido';
+        mensagem.classList.remove('msg-certo');
+        mensagem.classList.add('msg-erro');
+    }else if(escolhido.length == 0){
+        mensagem.innerHTML = 'Por Favor, Digite Um Número';
+        mensagem.classList.remove('msg-certo');
+        mensagem.classList.add('msg-erro');
+    }else{
+        var parametro1 = valorFinalSelecionado - valorInicialSelecionado;
+        console.log(parametro1);
+    
+        var parametro2 = valorInicialSelecionado;
+        console.log(parametro2);
+    
+        var resultado = Math.floor(Math.random() * parseInt(parametro1)) + parseInt(parametro2);
+        console.log('resultado: ' + resultado);
+        
+        if(escolhido == resultado){
+            mensagem.innerHTML = 'Você Acertou! Parabéns';
+            mensagem.classList.remove('msg-erro');
+            mensagem.classList.add('msg-certo');
+        }else{
+            mensagem.innerHTML = 'Você Errou! Tente Novamente';
+            mensagem.classList.remove('msg-certo');
+            mensagem.classList.add('msg-erro');
+        }
+    }
 }
